667268427437fe5f9adb0dd1d0b57dd3f0a7966e
alitaso345 <alice.maru345@gmail.com>
2020-05-23 02:50:43 +0900

    terminated

--- 4-3/main.go
+++ 4-3/main.go
@@ -1,19 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
-	doWork := func(strings <-chan string) <-chan interface{} {
-		completed := make(chan interface{})
+	doWork := func(done <-chan interface{}, strings <-chan string) <-chan interface{} {
+		terminated := make(chan interface{})
 		go func() {
-			defer fmt.Println("doWork exited.")
-			for s := range strings {
-				fmt.Println(s)
+			defer fmt.Println("do work exited.")
+			defer close(terminated)
+			for {
+				select {
+				case s := <-strings:
+					fmt.Println(s)
+				case <-done:
+					return
+				}
 			}
 		}()
-		return completed
+		return terminated
 	}
 
-	doWork(nil)
+	done := make(chan interface{})
+	terminated := doWork(done, nil)
+
+	go func() {
+		time.Sleep(1 * time.Second)
+		fmt.Println("Canceling doWork goroutine...")
+		close(done)
+	}()
+
+	<-terminated
 	fmt.Println("Done.")
 }
