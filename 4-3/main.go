8cb14bb4671a313985d149b60d683144c27dc1c4
alitaso345 <alice.maru345@gmail.com>
2020-05-23 23:18:06 +0900

    blocking

--- 4-3/main.go
+++ 4-3/main.go
@@ -2,36 +2,26 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"math/rand"
 )
 
 func main() {
-	doWork := func(done <-chan interface{}, strings <-chan string) <-chan interface{} {
-		terminated := make(chan interface{})
+	newRandStream := func() <-chan int {
+		randStream := make(chan int)
 		go func() {
-			defer fmt.Println("do work exited.")
-			defer close(terminated)
+			defer fmt.Println("newRandStream closure exited.")
+			defer close(randStream)
 			for {
-				select {
-				case s := <-strings:
-					fmt.Println(s)
-				case <-done:
-					return
-				}
+				randStream <- rand.Int()
 			}
 		}()
-		return terminated
+
+		return randStream
 	}
 
-	done := make(chan interface{})
-	terminated := doWork(done, nil)
-
-	go func() {
-		time.Sleep(1 * time.Second)
-		fmt.Println("Canceling doWork goroutine...")
-		close(done)
-	}()
-
-	<-terminated
-	fmt.Println("Done.")
+	randStream := newRandStream()
+	fmt.Println("3 random lists")
+	for i := 1; i <= 3; i++ {
+		fmt.Printf("%d: %d\n", i, <-randStream)
+	}
 }
