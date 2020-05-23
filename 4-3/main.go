74b08daac9c22e1f3394c8e21a4d91317b5a34d3
alitaso345 <alice.maru345@gmail.com>
2020-05-23 23:24:13 +0900

    close goroutine

--- 4-3/main.go
+++ 4-3/main.go
@@ -3,25 +3,33 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"time"
 )
 
 func main() {
-	newRandStream := func() <-chan int {
+	newRandStream := func(done <-chan interface{}) <-chan int {
 		randStream := make(chan int)
 		go func() {
 			defer fmt.Println("newRandStream closure exited.")
 			defer close(randStream)
 			for {
-				randStream <- rand.Int()
+				select {
+				case randStream <- rand.Int():
+				case <-done:
+					return
+				}
 			}
 		}()
 
 		return randStream
 	}
 
-	randStream := newRandStream()
+	done := make(chan interface{})
+	randStream := newRandStream(done)
 	fmt.Println("3 random lists")
 	for i := 1; i <= 3; i++ {
 		fmt.Printf("%d: %d\n", i, <-randStream)
 	}
+	close(done)
+	time.Sleep(1 * time.Second)
 }
